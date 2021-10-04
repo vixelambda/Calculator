f20ec64ba5b7511ffe67975fcb8d2b8999a8e635
vixelambda <dir.victor@yandex.ru>
2021-10-04 19:45:13 +0300

    Create console interface

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -4,19 +4,77 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator obj = new Calculator(10, 100);
-        System.out.println(obj.getX() + " " + obj.getY());
-        System.out.println("Введите 2 числа:");
+        System.out.println("Выберите действие: \n" +
+                "'+' - Сложение\n" +
+                "'-' - Вычитание\n" +
+                "'*' - Умножение\n" +
+                "'/' - Деление\n" +
+                "'1' - Начислить НДС\n" +
+                "'2' - Выделить НДС\n" +
+                "'3' - Расчет взносов на з/п");
         Scanner inp = new Scanner(System.in);
-        double num1 = inp.nextDouble();
-        double num2 = inp.nextDouble();
-        obj.setX(num1);
-        obj.setY(num2);
-        System.out.println("Сумма чисел: " + obj.add());
-        System.out.println("Разность чисел: " + obj.sub());
-        System.out.println("Произведение чисел: " + obj.mult());
-        System.out.println("Частное чисел: " + obj.div());
-        System.out.println("Начисленный НДС: " + obj.add_nds());
-        System.out.println("Выделенный НДС: " + obj.sub_nds());
+        String ch = inp.nextLine();
+        boolean b = true;
+        double num1 = 0, num2 = 0;
+        while (b) {
+            if (ch.equals("3")) {
+                System.out.println("Введите размер заработной платы:");
+                num1 = inp.nextDouble();
+                b = false;
+            }
+            else if (ch.equals("1") || ch.equals("2")) {
+                System.out.println("Введите сумму и ставку(%):");
+                num1 = inp.nextDouble();
+                num2 = inp.nextDouble();
+                b = false;
+            }
+            else if (ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/")) {
+                System.out.println("Введите первое и второе число:");
+                num1 = inp.nextDouble();
+                num2 = inp.nextDouble();
+                b = false;
+            }
+            else {
+                System.out.println("Введена неправильная операция, повторите попытку");
+                inp = new Scanner(System.in);
+                ch = inp.nextLine();
+            }
+        }
+        Calculator c1 = new Calculator(num1,num2);
+        System.out.print("Результат: ");
+        switch (ch) {
+            case "+":
+                System.out.println(c1.getX() + " + " + c1.getY() + " = " + c1.add());
+                break;
+            case "-":
+                System.out.println(c1.getX() + " - " + c1.getY() + " = " + c1.sub());
+                break;
+            case "*":
+                System.out.println(c1.getX() + " * " + c1.getY() + " = " + c1.mult());
+                break;
+            case "/":
+                System.out.println(c1.getX() + " / " + c1.getY() + " = " + c1.div());
+                break;
+            case "1":
+                System.out.println("НДС = " + c1.add_nds());
+                System.out.println("Сумма вместе с налогом = " + (c1.getX() + c1.add_nds()));
+                break;
+            case "2":
+                System.out.println("НДС = " + c1.sub_nds());
+                System.out.println("Сумма без НДС = " + (c1.getX() - c1.sub_nds()));
+                break;
+            case "3":
+                c1.setY(22);
+                System.out.println("на ОПС = " + c1.add_nds());
+                c1.setY(5.1);
+                System.out.println("на ОМС = " + c1.add_nds());
+                c1.setY(2.9);
+                System.out.println("на ВНиМ = " + c1.add_nds());
+                c1.setY(30);
+                System.out.println("Общий взнос = " + c1.add_nds());
+                break;
+            default:
+                System.out.println("Неправильная операция");
+        }
     }
 }
