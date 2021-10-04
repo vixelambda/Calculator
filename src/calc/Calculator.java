0236c7b64e68177d4e54fd5fe501b895867c6a79
vixelambda <dir.victor@yandex.ru>
2021-10-04 13:17:33 +0300

    Create calculator fields

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -1,5 +1,26 @@
 package calc;
 
 public class Calculator {
+    private double x, y;
 
+    public Calculator(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
--- src/calc/Main.java
+++ src/calc/Main.java
@@ -2,6 +2,7 @@ package calc;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Calculator obj = new Calculator(10, 100);
+        System.out.println(obj.getX() + " " + obj.getY());
     }
 }
